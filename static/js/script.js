4ec256c92b543fdb5942074ae9b9d67fdb752ce4
alexeykuz-sys <alexeykuz@gmail.com>
2021-02-13 16:01:02 +0000

    add catergory

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,20 @@ $(document).ready(function(){
   });
 
   
-$(document).ready(function(){
-    $('.collapsible').collapsible();
+
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+     $("select").formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
 
-  $(document).ready(function(){
-    $('.tooltipped').tooltip();
-  });
-        
